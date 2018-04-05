de110a109f7aad488a53da5dae87bdaadc6e1a09
zhangzhangrp <995405279@qq.com>
2018-04-05 23:10:24 +0800

    reg

--- src/components/person/Reg.vue
+++ src/components/person/Reg.vue
@@ -1,7 +1,7 @@
 <template>
   <div  class="con" id="login">
-      <p><input type="text" placeholder="请输入用户名"></p> 
-      <p><input type="password" placeholder="请输入密码"></p>
+      <p><input name="reg_name" v-model="reg_name" type="text" placeholder="请输入用户名"></p> 
+      <p><input name="reg_pwd" v-model="reg_pwd" type="password" placeholder="请输入密码"></p>
       <p><input type="submit" value="注册" @click="register"></p>
   </div>
 </template>
@@ -10,10 +10,19 @@
 import Axios from "axios";
 import VueResource from "vue-resource";
 export default {
+  data(){
+    return{
+      reg_name:'',
+      reg_pwd:''
+    }
+  },
   methods: {
     register() {
-      Axios.get("")
-        .then()
+       let data={"reg_name":this.reg_name,"reg_pwd":this.reg_pwd};
+      Axios.get("http://localhost/appci/index.php/user/reg",{params:data})
+        .then(res=>{
+               console.log(res);
+        })
         .catch();
     }
   }
